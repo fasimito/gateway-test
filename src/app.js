b09bba4d8b02ea9e180eda805742f0f6c3663c90
陈春勇 <815472970@qq.com>
2018-04-24 18:04:44 +0800

    修改log 信息

--- src/app.js
+++ src/app.js
@@ -12,13 +12,16 @@ app.listen(3000, () => {
 
 // fork一个子进程，用于监听servie 列表变化
 const workerProcess = child_process.fork('src/startWatch.js');
+
+// 子进程退出
 workerProcess.on('exit', function (code) {
     forkDebug(`子进程已退出，退出码：${code}`);
 });
 workerProcess.on('error', function (code) {
-    forkDebug('error: ' + code);
+    forkDebug(`error: ${code}`);
 });
-
+// fork线程中接收到数据
 workerProcess.on('message', msg => {
     appDebug(`从监控中数据变化：${JSON.stringify(msg)}`);
+    //通知缓存中service列表变化
 })
